781021e37585ba47d54b3f4912e67eeabab3796b
MGupta28 <guptamanan282@gmail.com>
2021-12-29 22:20:13 +0530

    distance correction in fun obstacle avoid and added angular velocity to bot

--- scripts/obstacle_avoider.py
+++ scripts/obstacle_avoider.py
@@ -19,13 +19,13 @@ class obstacle_avoider:
             }
         rospy.init_node('obstacle_avoider')
         rospy.Subscriber('/laser/scan', LaserScan, self.laser_callback)
-        self.dist = 0.5
-        self.wall_dist = 0.3
+        self.dist = 0.7
+        self.wall_dist = 0.5
         self.avoid = False
 
     def turn(self, z):
         self.message
-        self.message.linear.x = 0.5
+        self.message.linear.x = 0.2
         self.message.angular.z = z
         self.pub.publish(self.message)
 
@@ -42,18 +42,18 @@ class obstacle_avoider:
     def obstacle_avoid(self):
 
         while self.avoid != True:
-            if self.regions['fright'] < self.dist and self.regions['front'] < self.dist and self.regions['fleft'] < self.dist: #no wall detected
+            if self.regions['fright'] > self.dist and self.regions['front'] > self.dist and self.regions['fleft'] > self.dist: #no wall detected
                 print("Wall Avoided")
                 self.avoid = True
-            elif self.regions['fright'] > self.dist and self.regions['front'] < self.dist and self.regions['fleft'] < self.dist: #wall on front-right
+            elif self.regions['fright'] < self.dist and self.regions['front'] > self.dist and self.regions['fleft'] > self.dist: #wall on front-right
                 print("Turning Left")
                 self.turn(math.pi/4)
                 self.avoid = False
-            elif self.regions['fright'] < self.dist and self.regions['front'] > self.dist and self.regions['fleft'] < self.dist: #wall in front
+            elif self.regions['fright'] > self.dist and self.regions['front'] < self.dist and self.regions['fleft'] > self.dist: #wall in front
                 print("Turning left")
                 self.turn(math.pi/2)
                 self.avoid = False
-            elif self.regions['fright'] < self.dist and self.regions['front'] < self.dist and self.regions['fleft'] > self.dist: #wall on front-left
+            elif self.regions['fright'] > self.dist and self.regions['front'] > self.dist and self.regions['fleft'] < self.dist: #wall on front-left
                 print("Turning right")
                 self.turn(-math.pi/4)
                 self.avoid = False
@@ -64,8 +64,8 @@ class obstacle_avoider:
             elif self.regions['fright'] > self.dist or self.regions['front'] < self.dist or self.regions['fleft'] < self.dist: #wall either in front and front-left
                 print("Turning right")
                 self.turn(-math.pi/4)
-                self. avoid = False
-
+                self.avoid = False   
+        
         if self.avoid == True:
             #call the goto function as the wall is avoided
             print("calling the goto function now")
@@ -74,9 +74,10 @@ class obstacle_avoider:
         print("front = " + str(self.regions['front']))
         print("fleft = " + str(self.regions['fleft']))
         print("fright = " + str(self.regions['fright']))
-        if self.regions['front'] < self.wall_dist:
+        if self.regions['front'] < self.wall_dist or self.regions['fleft'] < self.wall_dist or self.regions['fright'] < self.wall_dist:
             print("obstacle detected")
             self.message.linear.x = 0
+            #self.message.angular.z = 0
             self.pub.publish(self.message)
             self.obstacle_avoid()
 
@@ -86,7 +87,8 @@ class obstacle_avoider:
         self.rate = rospy.Rate(15)
 
         while not rospy.is_shutdown():
-            self.message.linear.x = 0.5
+            self.message.linear.x = 0.3
+            self.message.angular.z = 0.0
             self.pub.publish(self.message)
             self.check_obstacle()
             self.rate.sleep()
@@ -95,4 +97,4 @@ class obstacle_avoider:
 if __name__ == "__main__":
     avoider = obstacle_avoider()
     avoider.demo_controller()
-    rospy.spin()
+    #rospy.spin()
