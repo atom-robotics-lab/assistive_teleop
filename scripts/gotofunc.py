2461e30292f7493deac1b4fcf20f9fcf02104e9b
akashshar-ma <mr.robot191121@gmail.com>
2021-12-30 21:05:13 +0530

    fixed all errors ,added wait message and fixed odom callback

--- scripts/gotofunc.py
+++ scripts/gotofunc.py
@@ -4,8 +4,6 @@ from geometry_msgs.msg import Point,Twist
 from nav_msgs.msg import Odometry
 import numpy as np
 from tf.transformations import euler_from_quaternion
-from math import atan2
-
 class Robot_Controller:
     #initialised values 
     def __init__(self):
@@ -14,7 +12,7 @@ class Robot_Controller:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('/odom', Odometry, self.odom_callback)
         self.pose = []
-        self.state = 4
+        self.state = 0
         self.velocity_msg = Twist()
 
 #obstace avoidence function
@@ -23,10 +21,10 @@ class Robot_Controller:
 
     #odmom callack function
     def odom_callback(self,data):
-      x  = data.pose.pose.orientation
-      y  = data.pose.pose.orientation
-      z = data.pose.pose.orientation
-      w = data.pose.pose.orientation
+      x  = data.pose.pose.orientation.x
+      y  = data.pose.pose.orientation.y
+      z = data.pose.pose.orientation.z
+      w = data.pose.pose.orientation.w
       self.pose = [data.pose.pose.position.x, data.pose.pose.position.y, euler_from_quaternion([x,y,z,w])[2]]
     #move function to move robot
     def move(self,linear,angular):
@@ -53,11 +51,10 @@ class Robot_Controller:
       3) state = 2; goal reached '''   
      theta_precision = 0.16  
      dist_precision = 0.35
+     #wait for message
+     rospy.wait_for_message("/odom",Odometry)
      while self.state != 2:
-        # theta_goal = np.arctan((dest_y - self.pose[1])/(dest_x - self.pose[0]))   #slope
-        inc_x=dest_x -self.pose[0]
-        inc_y=dest_y-self.pose[1]
-        theta_goal=atan2(inc_y,inc_x)
+        theta_goal = np.arctan((dest_y - self.pose[1])/(dest_x - self.pose[0]))   #slope
         if theta_goal>0:
             theta_goal+=0.04
         elif theta_goal<0:
@@ -69,7 +66,7 @@ class Robot_Controller:
         if self.state == 0:
             # if theta_error is greated than the required precision then fix the yaw by rotating the bot
             # if required precision is reached then change current state to 1
-            if np.abs(theta_error) > self.theta_precision:   
+            if np.abs(theta_error) > theta_precision:   
                 rospy.loginfo("Fixing Yaw")
                 self.fix_yaw(theta_error, 1.7)
             else:
@@ -90,12 +87,12 @@ class Robot_Controller:
                 self.state = 0
             elif position_error < dist_precision:
                 rospy.loginfo("GOAL REACHED")
+                self.move(0,0)
                 self.state=2
 
 
 if __name__ == "__main__":
     Robot = Robot_Controller()
-    Robot.goto(5,5)
-    # Robot.move_straight(12,0.8)
-    # Robot.move(0,5)
+    Robot.goto(3,2)
+    
            
