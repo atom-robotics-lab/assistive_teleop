62377678860fe201dd09991c35add497b3daf8f8
MGupta28 <guptamanan282@gmail.com>
2022-01-02 17:25:06 +0530

    replaced the turn function with move function

--- scripts/obstacle_avoider.py
+++ scripts/obstacle_avoider.py
@@ -20,13 +20,11 @@ class obstacle_avoider:
         rospy.init_node('obstacle_avoider')
         rospy.Subscriber('/laser/scan', LaserScan, self.laser_callback)
         self.dist = 1.0
+        self.dist2 = 1.131
         self.wall_dist = 0.8
         self.avoid = False
         self.message = Twist()
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-
-    def turn(self, z):
-        self.move(0.05 , z)
 
     def laser_callback(self, msg): 
         self.regions = { 
@@ -35,42 +33,52 @@ class obstacle_avoider:
             'front':  min(min(msg.ranges[0:24] + msg.ranges[336:359]), 10), 
             'fleft':  min(min(msg.ranges[25:75]), 10), 
             'left':   min(min(msg.ranges[76:125]), 10), 
-            } 
-        #print(self.regions)
+            }         
 
     def obstacle_avoid(self):
         print("obstacle avoider started")
         self.avoid = False
 
         while self.avoid == False:
-            print("into while loop")
-            if self.regions['fright'] > self.dist and self.regions['front'] > self.dist and self.regions['fleft'] > self.dist: #no wall detected
+            print(self.regions)
+            #print("into while loop")
+            if self.regions['fright'] > self.dist2 and self.regions['front'] > self.dist and self.regions['fleft'] > self.dist2: #no wall detected
                 print("Wall Avoided")
                 self.avoid = True
-            elif self.regions['fright'] < self.dist and self.regions['front'] > self.dist and self.regions['fleft'] > self.dist: #wall on front-right
+            elif self.regions['fright'] < self.dist2 and self.regions['front'] > self.dist and self.regions['fleft'] > self.dist2: #wall on front-right
                 print("Turning Left for front-right")
-                self.turn(1.2)
+                self.move(0.1, 0.3)
                 self.avoid = False
-            elif self.regions['fright'] > self.dist and self.regions['front'] < self.dist and self.regions['fleft'] > self.dist: #wall in front
+            elif self.regions['fright'] > self.dist2 and self.regions['front'] < self.dist and self.regions['fleft'] > self.dist2: #wall in front
                 print("Turning left for front")
-                self.turn(1.2)
+                self.move(0.1, 0.3)
                 self.avoid = False
-            elif self.regions['fright'] > self.dist and self.regions['front'] > self.dist and self.regions['fleft'] < self.dist: #wall on front-left
+            elif self.regions['fright'] > self.dist2 and self.regions['front'] > self.dist and self.regions['fleft'] < self.dist2: #wall on front-left
                 print("Turning right for front-left")
-                self.turn(-1.2)
+                self.move(0.1, -0.3)
                 self.avoid = False
-            elif self.regions['fright'] < self.dist or self.regions['front'] < self.dist or self.regions['fleft'] > self.dist: #wall either in front and front-right
+            elif self.regions['fright'] < self.dist2 and self.regions['front'] < self.dist and self.regions['fleft'] > self.dist2: #wall in front and front-right
                 print("Turning left for front and front-right")
-                self.turn(1.2)
+                self.move(0.1, 0.3)
                 self.avoid = False
-            elif self.regions['fright'] > self.dist or self.regions['front'] < self.dist or self.regions['fleft'] < self.dist: #wall either in front and front-left
+            elif self.regions['fright'] > self.dist2 and self.regions['front'] < self.dist and self.regions['fleft'] < self.dist2: #wall in front and front-left
                 print("Turning right for front and front-left")
-                self.turn(-1.2)
-                self.avoid = False   
+                self.move(0.1, -0.3)
+                self.avoid = False 
+            elif self.regions['fright'] < self.dist2 and self.regions['front'] < self.dist and self.regions['fleft'] < self.dist2: #wall everywhere
+                if self.regions['fleft'] < self.regions['fright']:
+                    print("Turning right for front and front-left and ront-right")
+                    self.move(0.1, -0.3)
+                    self.avoid = False   
+                elif self.regions['fleft'] > self.regions['fright']:
+                    print("Turning left for front and front-left and ront-right")
+                    self.move(0.1, 0.3)
+                    self.avoid = False
         
         if self.avoid == True:
             #call the goto function as the wall is avoided
             print("calling the goto function now")
+            self.demo_controller()
 
     def check_obstacle(self):
         print("front = " + str(self.regions['front']))
