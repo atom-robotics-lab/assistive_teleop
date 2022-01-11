a6832d1584239fd23a603248a895145e85cf505f
MGupta28 <guptamanan282@gmail.com>
2022-01-11 19:55:07 +0530

    complete code moved into the class follow_wall

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -9,125 +9,117 @@ from tf import transformations
 
 import math
 
-pub_ = None
-regions_ = {
-    'right': 0,
-    'fright': 0,
-    'front': 0,
-    'fleft': 0,
-    'left': 0,
-}
-state_ = 0
-state_dict_ = {
-    0: 'find the wall',
-    1: 'turn left',
-    2: 'follow the wall',
-}
-def clbk_laser(msg):
-    global regions_
-    regions_ = {
-        'right':  min(min(msg.ranges[235:285]), 10), 
-        'fright': min(min(msg.ranges[286:335]), 10), 
-        'front':  min(min(msg.ranges[0:24] + msg.ranges[336:359]), 10), 
-        'fleft':  min(min(msg.ranges[25:75]), 10), 
-        'left':   min(min(msg.ranges[76:125]), 10),
-    }
+class follow_wall:
 
-    take_action()
+    def __init__(self):
+        self.pub_ = None
+        self.regions = {
+            'right': 0,
+            'fright': 0,
+            'front': 0,
+            'fleft': 0,
+            'left': 0,
+        }
+        self.state_ = 0
+        self.state_dict_ = {
+            0: 'find the wall',
+            1: 'turn left',
+            2: 'follow the wall',
+        }
+        self.message = Twist()
+        self.d = 1.0
+        rospy.init_node('reading_laser')
+        self.pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)        
+        self.sub = rospy.Subscriber('/laser/scan', LaserScan, self.clbk_laser)
 
+    def clbk_laser(self, msg):
+        self.regions = {
+            'right':  min(min(msg.ranges[235:285]), 10), 
+            'fright': min(min(msg.ranges[286:335]), 10), 
+            'front':  min(min(msg.ranges[0:24] + msg.ranges[336:359]), 10), 
+            'fleft':  min(min(msg.ranges[25:75]), 10), 
+            'left':   min(min(msg.ranges[76:125]), 10),
+        }
+        self.take_action()
 
-def change_state(state):
-    global state_, state_dict_
-    if state is not state_:
-        print ('Wall follower - [%s] - %s' % (state, state_dict_[state]))
-        state_ = state
+    def change_state(self, state):
+        self.state_
+        self.state_dict_
+        if state is not self.state_:
+            print ('Wall follower - [%s] - %s' % (state, self.state_dict_[state]))
+            self.state_ = state
 
-def take_action():
-    global regions_, msg, linear_x, angular_z, state_description
-    regions = regions_
-    msg = Twist()
-    linear_x = 0
-    angular_z = 0
-    
-    state_description = ''
-    
-    d = 1.0
-    
-    if regions['front'] > d and regions['fleft'] > d and regions['fright'] > d:
-        state_description = 'case 1 - nothing'
-        change_state(0)
-    elif regions['front'] < d and regions['fleft'] > d and regions['fright'] > d:
-        state_description = 'case 2 - front'
-        change_state(1)
-    elif regions['front'] > d and regions['fleft'] > d and regions['fright'] < d:
-        state_description = 'case 3 - fright'
-        change_state(2)
-    elif regions['front'] > d and regions['fleft'] < d and regions['fright'] > d:
-        state_description = 'case 4 - fleft'
-        change_state(0)
-    elif regions['front'] < d and regions['fleft'] > d and regions['fright'] < d:
-        state_description = 'case 5 - front and fright'
-        change_state(1)
-    elif regions['front'] < d and regions['fleft'] < d and regions['fright'] > d:
-        state_description = 'case 6 - front and fleft'
-        change_state(1)
-    elif regions['front'] < d and regions['fleft'] < d and regions['fright'] < d:
-        state_description = 'case 7 - front and fleft and fright'
-        change_state(1)
-    elif regions['front'] > d and regions['fleft'] < d and regions['fright'] < d:
-        state_description = 'case 8 - fleft and fright'
-        change_state(0)
-    else:
-        state_description = 'unknown case'
-        rospy.loginfo(regions)
-
-def find_wall():
-    msg = Twist()
-    msg.linear.x = 0.2
-    msg.angular.z = -0.3
-    return msg
-
-def turn_left():
-    msg = Twist()
-    msg.angular.z = 0.3
-    return msg
-
-def follow_the_wall():
-    global regions_
-    
-    msg = Twist()
-    msg.linear.x = 0.5
-    return msg
-
-def main():
-    global pub_, sub
-    
-    rospy.init_node('reading_laser')
-    
-    pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-    
-    sub = rospy.Subscriber('/laser/scan', LaserScan, clbk_laser)
-    
-    rate = rospy.Rate(15)
-    while not rospy.is_shutdown():
-        print("front :" + str(regions_['front']))
-        print("fright :" + str(regions_['fright']))
-        print("fleft :" + str(regions_['fleft']))
-        msg = Twist()
-        if state_ == 0:
-            msg = find_wall()
-        elif state_ == 1:
-            msg = turn_left()
-        elif state_ == 2:
-            msg = follow_the_wall()
-            pass
+    def take_action(self):
+        state_description = ''
+        
+        if self.regions['front'] > self.d and self.regions['fleft'] > self.d and self.regions['fright'] > self.d:
+            state_description = 'case 1 - nothing'
+            self.change_state(0)
+        elif self.regions['front'] < self.d and self.regions['fleft'] > self.d and self.regions['fright'] > self.d:
+            state_description = 'case 2 - front'
+            self.change_state(1)
+        elif self.regions['front'] > self.d and self.regions['fleft'] > self.d and self.regions['fright'] < self.d:
+            state_description = 'case 3 - fright'
+            self.change_state(2)
+        elif self.regions['front'] > self.d and self.regions['fleft'] < self.d and self.regions['fright'] > self.d:
+            state_description = 'case 4 - fleft'
+            self.change_state(0)
+        elif self.regions['front'] < self.d and self.regions['fleft'] > self.d and self.regions['fright'] < self.d:
+            state_description = 'case 5 - front and fright'
+            self.change_state(1)
+        elif self.regions['front'] < self.d and self.regions['fleft'] < self.d and self.regions['fright'] > self.d:
+            state_description = 'case 6 - front and fleft'
+            self.change_state(1)
+        elif self.regions['front'] < self.d and self.regions['fleft'] < self.d and self.regions['fright'] < self.d:
+            state_description = 'case 7 - front and fleft and fright'
+            self.change_state(1)
+        elif self.regions['front'] > self.d and self.regions['fleft'] < self.d and self.regions['fright'] < self.d:
+            state_description = 'case 8 - fleft and fright'
+            self.change_state(0)
         else:
-            rospy.logerr('Unknown state!')
+            state_description = 'unknown case'
+            rospy.loginfo(self.regions)
+
+    def find_wall(self):
+        msg = Twist()
+        msg.linear.x = 0.2
+        msg.angular.z = -0.3
+        return msg
+
+    def turn_left(self):
+        msg = Twist()
+        msg.angular.z = 0.3
+        return msg
+
+    def follow_the_wall(self):
+        self.regions
         
-        pub_.publish(msg)
+        msg = Twist()
+        msg.linear.x = 0.5
+        return msg
+
+    def _state_(self):
         
-        rate.sleep()
+        rate = rospy.Rate(15)
+        while not rospy.is_shutdown():
+            print("front :" + str(self.regions['front']))
+            print("fright :" + str(self.regions['fright']))
+            print("fleft :" + str(self.regions['fleft']))
+            msg = Twist()
+            if self.state_ == 0:
+                msg = self.find_wall()
+            elif self.state_ == 1:
+                msg = self.turn_left()
+            elif self.state_ == 2:
+                msg = self.follow_the_wall()
+                pass
+            else:
+                rospy.logerr('Unknown state!')
+            
+            self.pub_.publish(msg)
+            
+            rate.sleep()
 
 if __name__ == '__main__':
-    main()
-
+    obs_state = follow_wall()
+    obs_state._state_()
