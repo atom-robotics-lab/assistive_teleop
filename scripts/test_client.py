c7af0cf8d54f6f04aec92b9da68b36ff9c864c07
MGupta28 <guptamanan282@gmail.com>
2022-01-30 18:22:31 +0530

    indentation corrected

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -7,13 +7,13 @@ import actionlib
 
 from assistive_teleop.msg import AvoidObstacleAction, AvoidObstacleActionGoal
 
-  if __name__ == '__main__':
-      rospy.init_node('Obstacle_Client_test')
-      client = actionlib.SimpleActionClient('Avoid_Obstacle', AvoidObstacleAction)
-      client.wait_for_server()
+if __name__ == '__main__':
+  rospy.init_node('Obstacle_Client_test')
+  client = actionlib.SimpleActionClient('Avoid_Obstacle', AvoidObstacleAction)
+  client.wait_for_server()
+  goal = AvoidObstacleActionGoal()
+  goal.angle = 5
+  # Fill in the goal here
+  client.send_goal(goal)
+  client.wait_for_result(rospy.Duration.from_sec(5.0))
   
-      goal = AvoidObstacleActionGoal()
-      goal.angle = 5
-      # Fill in the goal here
-      client.send_goal(goal)
-      client.wait_for_result(rospy.Duration.from_sec(5.0))
