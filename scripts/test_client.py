263c42f7822486c64f657929d0f823ee1b8ef859
namikxgithub <namanmalik0210@gmail.com>
2022-02-01 08:43:35 +0530

    completed sripts for test_server and test_client (minor tweeks required)

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -1,19 +1,50 @@
-
 #! /usr/bin/env python3
 
 import roslib
 import rospy
 import actionlib
 
-from assistive_teleop.msg import AvoidObstacleAction, AvoidObstacleActionGoal
+from assistive_teleop.msg import AvoidObstacleAction, AvoidObstacleGoal, AvoidObstacleFeedback
+
+class drive_client:
+
+  def __init__(self):
+
+    self.ob_status = False
+    self.client = actionlib.SimpleActionClient('Avoid_Obstacle', AvoidObstacleAction)
+    print('waiting for server')
+    self.client.wait_for_server()
+    print('server started')
+    goal = AvoidObstacleGoal()
+    goal.angle = 5
+    self.client.send_goal(goal, feedback_cb = self.object_cb)
+
+  
+  def object_cb(self, feedback):
+    self.ob_status = feedback.obstacle_clearance
+
+  def drive_robot(self):
+    rate = rospy.Rate(100)
+    drive_task = 0
+
+    while drive_task != 10:
+      if self.ob_status:
+        print('stoped Object Avoidance in Progress')
+        self.client.wait_for_result()
+
+      else:
+        drive_task += 1
+        print('task completed {}/10'.format(drive_task)) 
+      rospy.sleep(2)
+
+      rate.sleep()
+
+
+  
 
 if __name__ == '__main__':
   rospy.init_node('Obstacle_Client_test')
-  client = actionlib.SimpleActionClient('Avoid_Obstacle', AvoidObstacleAction)
-  client.wait_for_server()
-  goal = AvoidObstacleActionGoal()
-  goal.angle = 5
-  # Fill in the goal here
-  client.send_goal(goal)
-  client.wait_for_result(rospy.Duration.from_sec(5.0))
+  robot_driver = drive_client()
+  robot_driver.drive_robot()
+
   
--- scripts/test_server.py
+++ scripts/test_server.py
@@ -1,23 +1,45 @@
-
 #! /usr/bin/env python3
 
 import roslib
 import rospy
 import actionlib
 
-from assistive_teleop.msg import AvoidObstacleAction
+from assistive_teleop.msg import AvoidObstacleAction, AvoidObstacleFeedback, AvoidObstacleResult
 
 class ObstacleAvoidanceServer:
   def __init__(self):
     self.server = actionlib.SimpleActionServer('Avoid_Obstacle', AvoidObstacleAction, self.execute, False)
     self.server.start()
+    print('action server starting')
+    self.feedback = AvoidObstacleFeedback()
+    self.result = AvoidObstacleResult()
 
   def execute(self, goal):
-    # Do lots of awesome groundbreaking robot stuff here
+    rate = rospy.Rate(100)
+    index = 1
+    while True:
+      if index % 3 == 0:
+        self.feedback.obstacle_clearance = True
+        self.server.publish_feedback(self.feedback)
+        print('obstacle detected: {}'.format(index))
+        rospy.sleep(5)
+        print('obstacle avoided: {}'.format(index))
+        self.server.set_succeeded()
+        index +=1
+      
+      else:
+        print('no obstacle detected: {}'.format(index))
+        self.feedback.obstacle_clearance = False
+        self.server.publish_feedback(self.feedback)
+
+      rospy.sleep(2)
+      index +=1
+      rate.sleep()
+
     self.server.set_succeeded()
 
 
 if __name__ == '__main__':
   rospy.init_node('Obstacle_Server_test')
   server = ObstacleAvoidanceServer()
-  rospy.spin
+  rospy.spin()
